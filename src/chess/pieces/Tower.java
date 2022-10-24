69879597eb9c8138b9e8d34a3bad201334524f9c
abmaelvilar <abmaelvilar@gmail.com>
2022-10-24 20:12:45 -0300

    Criadas as peças torre e rei, adicionado o metodo de inicialização da partida

---
+++ src/chess/pieces/Tower.java
@@ -0,0 +1,20 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Tower extends ChessPiece{
+
+	public Tower(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "T";
+	}
+	
+		
+}
