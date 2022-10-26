2ba97984d3038bce0e23cdcb0a0117c53b2076e6
abmaelvilar <abmaelvilar@gmail.com>
2022-10-26 18:30:37 -0300

    correção de bug na logica de movimento da torre

--- src/chess/pieces/Tower.java
+++ src/chess/pieces/Tower.java
@@ -39,33 +39,35 @@ public class Tower extends ChessPiece{
 		
 		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow()-1);
+			p.setColumn(p.getColumn()-1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
 		//right
-		p.setValues(position.getRow()-1, position.getColumn()+1);
+		p.setValues(position.getRow(), position.getColumn()+1);
 		
 		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow()-1);
+			p.setColumn(p.getColumn()+1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		
-		//below
+		// below
 		p.setValues(position.getRow()+1, position.getColumn());
 		
 		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-			p.setRow(p.getRow()-1);
+			p.setRow(p.getRow()+1);
 		}
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
-		}		
+		}
+		
+		
 		return mat;		
 	}
 	
