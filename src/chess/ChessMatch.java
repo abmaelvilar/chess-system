0f4afa00ca211d9212ae1c993d11dd060d080f0e
abmaelvilar <abmaelvilar@gmail.com>
2022-10-29 14:40:23 -0300

    implementado o cavalo e seus movimentos

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.Bishop;
+import chess.pieces.Horse;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Tower;
@@ -65,12 +66,12 @@ public class ChessMatch {
 	
 	private void initialSetup() {
 		placeNewPiece('a',1,new Tower(board,Color.WHITE));
-		placeNewPiece('b',1,new Tower(board,Color.WHITE));
+		placeNewPiece('b',1,new Horse(board,Color.WHITE));
 		placeNewPiece('c',1,new Bishop(board,Color.WHITE));
 		placeNewPiece('d',1,new Tower(board,Color.WHITE));
 		placeNewPiece('e',1,new King(board,Color.WHITE));
 		placeNewPiece('f',1,new Bishop(board,Color.WHITE));
-		placeNewPiece('g',1,new Tower(board,Color.WHITE));
+		placeNewPiece('g',1,new Horse(board,Color.WHITE));
 		placeNewPiece('h',1,new Tower(board,Color.WHITE));		
 		placeNewPiece('a',2,new Pawn(board,Color.WHITE));
 		placeNewPiece('b',2,new Pawn(board,Color.WHITE));
@@ -83,12 +84,12 @@ public class ChessMatch {
 		
 		
 		placeNewPiece('a',8,new Tower(board,Color.BLACK));
-		placeNewPiece('b',8,new Tower(board,Color.BLACK));
+		placeNewPiece('b',8,new Horse(board,Color.BLACK));
 		placeNewPiece('c',8,new Bishop(board,Color.BLACK));
 		placeNewPiece('d',8,new Tower(board,Color.BLACK));
 		placeNewPiece('e',8,new King(board,Color.BLACK));
 		placeNewPiece('f',8,new Bishop(board,Color.BLACK));
-		placeNewPiece('g',8,new Tower(board,Color.BLACK));
+		placeNewPiece('g',8,new Horse(board,Color.BLACK));
 		placeNewPiece('h',8,new Tower(board,Color.BLACK));		
 		placeNewPiece('a',7,new Pawn(board,Color.BLACK));
 		placeNewPiece('b',7,new Pawn(board,Color.BLACK));
